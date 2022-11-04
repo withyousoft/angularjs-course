29d5e0123bf49465f3dab2d961cfd2d41da65b33
withyousoft1983 <withyousoft1@hotmail.com>
2022-11-04 06:49:35 -0400

    Complete pure JS version of name calculator

---
+++ app.js
@@ -0,0 +1,26 @@
+var student = {
+  name: "",
+  type: "student"
+};
+
+document.addEventListener("DOMContentLoaded", contentLoaded);
+
+function contentLoaded(event) {
+  document.getElementById("name").addEventListener("keyup", keyUp);
+}
+
+function keyUp(event) {
+  calculateNumericOutput();
+}
+
+function calculateNumericOutput() {
+  student.name = document.getElementById('name').value;
+  var totalNameValue = 0;
+
+  for (var i = 0; i < student.name.length; i++) {
+    totalNameValue += student.name.charCodeAt(i);
+  }
+
+  var output = "Total Numeric value of person's name is " + totalNameValue;
+  document.getElementById('output').innerText = output;
+}
